924eed2f8d9000416608ddc8fc2c2ed59f250960
jan-ove-erichsen <jan.erichsen@applift.com>
2019-03-03 19:18:04 +0100

    create api test

--- ml-app/api_endpoint/predict.py
+++ ml-app/api_endpoint/predict.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle    # Todo: consider joblib for scikit models instead
 import flask
 
 
@@ -10,21 +10,29 @@ def run_flask(model_file):
     model = pickle.load(open(model_file, "rb"))
 
     @app.route('/ml-app/v1.0/predict/', methods=['POST'])
-
     def predict():
         '''
         Tasks to execute upon each API call
         :return: 
         '''
 
-        feature_array = flask.request.get_json()['feature_array']
-        print('feature_array:', feature_array)
-        prediction = model.predict([feature_array]).tolist()
+        if flask.request.method == 'POST':
 
-        # preparing a response object and storing the model's predictions
-        response = {}
-        response['predictions'] = prediction
+            try:
+                data = flask.request.get_json()
+                print('feature_array:', data['feature_array'])
 
-        # sending our response object back as json
-        return flask.jsonify(response)
+            except ValueError:
+                return flask.jsonify("Post request failed")
 
+            prediction = model.predict(data['feature_array']).tolist()
+
+            # preparing a response object and storing the model's predictions
+            response = {}
+            response['predictions'] = prediction
+
+            # sending our response object back as json
+            return flask.jsonify(response)
+
+    # start the API
+    app.run(debug=True, port=5000)
