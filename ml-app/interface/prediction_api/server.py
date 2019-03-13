9750a9574a6002300b164dceef01fe07dae3ede9
jan-ove-erichsen <jan.erichsen@applift.com>
2019-03-14 00:08:06 +0100

    finalized api endpoint debugging

--- ml-app/interface/prediction_api/server.py
+++ ml-app/interface/prediction_api/server.py
@@ -4,16 +4,26 @@ import pandas as pd
 
 # Todo: consider extending to different formats, eg. byte streams
 
-def run(model_file):
+def run(model_file, endpoint, probability = False, debug=True, port=5000):
+    '''
+    Starts server, exposing specific enpoint on specific port and loads scikit learn 
+    model from pickled object into memory
+    :param model_file: String, path to pickled model
+    :param endpoint: String, example: /ml-app/v1.0/predict/
+    :param probability: Boolean, whether scikit returns probability or not
+    :param debug: Boolean, whether flask should run in debug mode or not
+    :param port: Integer, Which port to choose
+    :return: json, predictions and status code
+    '''
 
     app = flask.Flask(__name__)
 
     # load model
     print('Loading model')
-    model = pickle.load(open(model_file, "rb"))
+    model_metadata = pickle.load(open(model_file, "rb"))
 
-    @app.route('/ml-app/v1.0/predict/', methods=['POST'])
-    def predict():
+    @app.route(endpoint, methods=['POST'])
+    def predict(model_metadata=model_metadata, probability=probability):
         '''
         Tasks to execute upon each API call
         :return: 
@@ -23,25 +33,29 @@ def run(model_file):
 
             # try:
             data = flask.request.get_json()
-            data = pd.read_json(data, orient='records')
-            print('Input data:', data)
+            print('New input data:', data)
 
-            # except ValueError:
-            #     return flask.jsonify("Post request failed")
+            data = pd.read_json(data, orient='records')
 
             print('Making predictions')
-            prediction = model.predict(data)
+            model = model_metadata['model']
+
+            if probability is False:
+                prediction = model.predict(data)
+            elif probability is True:
+                prediction = model.predict_proba(data)
 
             # preparing a response object and storing the model's predictions
-            prediction_series = list(pd.Series(prediction))
+            prediction_series = pd.Series(prediction)
 
             # sending our response object back as json
-            responses = flask.jsonify(predictions=prediction_series.to_json(orient="records"))
-            responses.status_code = 200
+            predictions = prediction_series.to_json(orient="records")
+            print('predictions', predictions)
+            responses = flask.jsonify(predictions)
 
             return responses
 
     # start the API
-    app.run(debug=True, port=5000)
+    app.run(debug=debug, port=port)
 
 
--- tests/predict/classification/py/predict_rf_classifier_run_server.py
+++ tests/predict/classification/py/predict_rf_classifier_run_server.py
@@ -8,19 +8,7 @@ from interface.prediction_api import server
 from sklearn.datasets import make_classification
 
 
-# generate batch data to post to api in json format:
-X, y = make_classification(n_samples=1, n_features=4, n_informative=2, n_redundant=0, random_state=0,
-                           shuffle=False)
-X = pd.DataFrame(X)
-input_data = X.to_json(orient='records')
-print('Clients payload:', input_data)
-
 
 ## start the server and pass test model
 model_file = '/Users/user/testing/ml-app/models/classifier/RF/test_model_1.pickle'
-server.run(model_file)
-
-
-# post test json to API endpoint
-response = requests.post("http://ml-app/v1.0/predict:5000/", json = input_data)
-print('Server response:', response.json())
+server.run(model_file, endpoint = '/ml-app/v1.0/predict/')
